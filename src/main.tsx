b2203c15de4d471ff18a3c7b04e846c3ca24a1cf
Ak56Ai <akchouhan131@gmail.com>
2025-06-29 04:43:27 -0500

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AuthProvider } from './contexts/AuthContext';
-import { DataProvider } from './contexts/DataContext';
 import App from './App.tsx';
 import './index.css';
 
@@ -12,9 +11,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
-        <DataProvider>
-          <App />
-        </DataProvider>
+        <App />
       </AuthProvider>
     </QueryClientProvider>
   </StrictMode>
